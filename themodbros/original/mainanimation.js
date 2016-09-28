ceba82c69d664aebadd474e642097124391664e1
EdwardR5 <erees.hk@gmail.com>
2016-09-28 13:58:07 +0800

    Removed all gaming information and moved to TheModBros site

--- themodbros/original/mainanimation.js
+++
@@ -1,6 +0,0 @@
-/*$(function() {
-    $("#content p a").click(function() {
-        var selector = this.id.substring(this.id.indexOf("_") + 1);
-        $("#" + selector).fadeIn().siblings("div").fadeOut();
-    });
-});*/
